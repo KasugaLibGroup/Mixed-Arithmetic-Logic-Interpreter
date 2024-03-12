c95325a0e7effa0b569cfb0be0677ed6f3347d01
MegumiKasuga <1926195023@qq.com>
2024-03-12 20:35:37 +0800

    update dependencies

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module Mixed.Arithmetic.Logic.Interpreter {
+    exports interpreter;
+
+    exports interpreter.compute.data;
+    exports interpreter.compute.infrastructure;
+    exports interpreter.compute.exceptions;
+    exports interpreter.compute.data.functions;
+
+    exports interpreter.logic.data;
+    exports interpreter.logic.infrastructure;
+    exports interpreter.logic.operations;
+    exports interpreter.logic.data.functions;
+}
